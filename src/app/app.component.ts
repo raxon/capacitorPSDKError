6d8e4fdbc9007420077339e536a42b2b6141e17a
raxon <r4x0nx@gmail.com>
2020-11-25 14:42:21 +0100

    x

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+declare var PESDK;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'examplecap';
+
+export class AppComponent implements OnInit{
+  
+  ngOnInit(): void {
+    PESDK.openEditor(
+      console.log,
+      console.log,
+      PESDK.loadResource('www/assets/anything.jpg')
+    );
+  }
+
 }
